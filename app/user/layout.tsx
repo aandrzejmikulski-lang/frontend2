fc3cc5f539e9b9a0612e8f3a2111621d177d5852
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-24 07:07:46 +0200

    Update layout.tsx

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,1 +1,24 @@
+import UserSidebar from "@/components/UserSidebar";
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div style={{ display: "flex" }}>
+      <UserSidebar />
+
+      <div
+        style={{
+          marginLeft: 240,
+          padding: 40,
+          width: "100%",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
