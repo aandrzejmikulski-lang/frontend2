7de737321e29d3e669ce1fd23cb45fa78d42691d
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-24 07:03:04 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+export const supabase = createClient(
+  process.env.https://owhxyoxswzcycmgluywv.supabase.co!,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93aHh5b3hzd3pjeWNtZ2x1eXd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NDg0MDEsImV4cCI6MjA5NTEyNDQwMX0.WV38FzTFmgnJWpe5QHDA5Gp9UgTeb3j8ldh5z9LGHcI!
+);
