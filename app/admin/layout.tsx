afb6599a45e9e7538afb36c80937ee43fc6fbfe6
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-24 07:04:39 +0200

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,1 +1,24 @@
+import AdminSidebar from "@/components/AdminSidebar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div style={{ display: "flex" }}>
+      <AdminSidebar />
+
+      <div
+        style={{
+          marginLeft: 260,
+          padding: 40,
+          width: "100%",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
